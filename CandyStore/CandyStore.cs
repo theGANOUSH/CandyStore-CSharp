9e96a9c8c69b8a53ea19ba20dc9896a065d07551
theGANOUSH <babaganoush45@gmail.com>
2015-04-19 22:52:14 -0400

    Intial Commit

---
+++ CandyStore/CandyStore.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CandyStore
+{
+    class CandyStore
+    {
+        static int[] knapsack(int n, int m)
+        {
+            int[] nKnapsack = new int[m + 1];
+
+            for (int i = 0; i < n; i++)
+            {
+
+
+            }
+                return nKnapsack;
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                string[] lines = System.IO.File.ReadAllLines(@"Input.txt");
+
+                Console.WriteLine(lines[0]);
+
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+                Console.ReadKey();
+            }
+        }
+    }
+}
